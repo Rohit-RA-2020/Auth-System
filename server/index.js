7fa7d2710a8f4b9ccc824d88fc37ea89d84f707e
Rohit Ranjan <ranjanrohit812@gmail.com>
2023-08-20 21:51:59 +0530

    Testing out :P

--- server/index.js
+++ server/index.js
@@ -1,24 +1,18 @@
-// create a basic express server
-// import express
 const express = require('express');
-// import path
-const path = require('path');
-// import body-parser
 const bodyParser = require('body-parser');
-// import cors
 const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3000;
 const pool = require('./queries');
 
+app.use(cors());
 
-// import routes
-//create an app
-const app = express();
-// set port
-const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // create get route
 app.get('/', (req, res) => {
-    pool.query('SELECT * FROM persons', (error, results) => {
+    pool.query('SELECT * FROM users', (error, results) => {
         if (error) {
             throw error;
         }
@@ -26,4 +20,27 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(port, () => {});
+
+app.post('/createUser', (req, res) => {
+    const {email, password, name} = req.body;
+
+    if((pool.query(`select COUNT(*) from users where email = ${email}`)) == 0) {
+        console.log("email does not exist")
+        res.status(201).send(`User added`);
+    } else {
+        console.log("email exists")
+        res.status(409).send(`User already exists`);
+    }
+});
+
+// app.post('/createUser', (req, res) => {
+//     const {email, password, name} = req.body;
+//     pool.query('INSERT INTO users (email, password, name) VALUES ($1, $2, $3)', [email, password, name], (error, results) => {
+//         if (error) {
+//             res.status(409).send(error);
+//         }
+//         res.send(`User added`);
+//     });
+// });
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
